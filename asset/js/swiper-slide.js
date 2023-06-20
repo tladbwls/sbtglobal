a25ce19ed1f044b26aab56059c269536b45b2bfc
tladbwls <115197534+tladbwls@users.noreply.github.com>
2023-06-20 17:31:55 +0900

    sapService

--- asset/js/swiper-slide.js
+++ asset/js/swiper-slide.js
@@ -1,21 +1,10 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
+    clickable: true,
   },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  loop:true,
 });
