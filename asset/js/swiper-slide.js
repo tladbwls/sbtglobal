0facc7b439c71227db072a6e8418779fdceadf50
tladbwls <115197534+tladbwls@users.noreply.github.com>
2023-06-23 13:57:48 +0900

    0623

--- asset/js/swiper-slide.js
+++ asset/js/swiper-slide.js
@@ -1,10 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
+  slidesPerView: 3,
   spaceBetween: 30,
-  centeredSlides: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-  loop:true,
 });
