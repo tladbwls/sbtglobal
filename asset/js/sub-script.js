bb13e9b168ec441a48fbfabf30a8cc638cbbb3aa
tladbwls <115197534+tladbwls@users.noreply.github.com>
2023-07-04 11:02:18 +0900

    photo

--- asset/js/sub-script.js
+++ asset/js/sub-script.js
@@ -1,3 +1,16 @@
+
+
+const buttonArr = document.querySelectorAll('#navbar .container li');
+
+
+for(let i = 0; i < buttonArr.length; i++){
+  buttonArr[i].addEventListener('click',function(e){
+    e.preventDefault();
+    document.querySelector('.box' + (i + 1)).scrollIntoView(true);
+  });
+}
+console.log(buttonArr);
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
@@ -5,6 +18,5 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+
 });
-
-
